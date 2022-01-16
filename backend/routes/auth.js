ff96131599807335dcfb774dc4b2d4fd6431b410
ShahabAthar25 <shahabathar25@gmail.com>
2022-01-16 08:40:38 +0500

    Add backend folder structure

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const controller = require("../controllers/authController");
-
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-router.post("/logout", controller.logout);
-
-module.exports = router;
--- frontend/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "tailwindcss/tailwind.css";
-
-function MyApp({ Component, pageProps, authenticated }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;

120c8cbcc50b7aa2342dfc3be87ecc16f28ef0ad
ShahabAthar25 <shahabathar25@gmail.com>
2022-02-05 12:40:58 +0500

    Add Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
